341a4b79e4fb26a0c3db338bb2d698ab787babbe
Federico Paolinelli <fpaoline@redhat.com>
2026-07-17 11:14:23 -0400

    Add a note about yq and error if not available
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

--- srv6raw/configimage/generate_config_image.sh
+++ srv6raw/configimage/generate_config_image.sh
@@ -17,6 +17,11 @@
 
 set -euo pipefail
 
+if ! echo '{}' | yq -y '.' > /dev/null 2>&1; then
+    echo "ERROR: python yq required (pip install yq)" >&2
+    exit 1
+fi
+
 SCRIPTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 APPLIANCE_CACHE="${SCRIPTDIR}/../appliance/cache"
 
